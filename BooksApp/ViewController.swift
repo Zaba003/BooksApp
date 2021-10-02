b1102089aa5b3ad13a1839d741eb54fd9a09d256
Kirill Zaborsky <avto.oil@gmail.com>
2021-10-02 10:10:18 +0700

    Initial Commit

---
+++ BooksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BooksApp
+//
+//  Created by Кирилл Заборский on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
